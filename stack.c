b56ed931f30819f13605b3f52c8d0788c3c024e6
zrygan <zr.gntn@gmail.com>
2024-06-07 23:30:27 +0800

    Removed unecessary comments

--- stack.c
+++ stack.c
@@ -11,25 +11,6 @@
 #include "stack.h"
 #include "libraries.h"
 
-/* Notes on STACKS
-    Operators:
-        - Push: insert "something" to stack
-        - Pop: delete "something" in the stack
-
-    Attributes:
-        - top: index of the most recently inserted element
-        - size: capacity of the stack
-*/
-
-/** createStack
- * creates an empty stack with size `S`
- * 
- * @params:
- *  - S : int : the size/capacity of the stack
- * 
- * @returns:
- *  - none
- */
 void create(int S){
 
 
