07bc4e4bb362d39d1829c9b5521f95a3f1cd90eb
zrygan <zr.gntn@gmail.com>
2024-06-07 23:25:31 +0800

    Added essential functions for the stack

--- stack.c
+++ stack.c
@@ -7,3 +7,30 @@
  * 
  * Source file of stack data structure.
 */
+
+#include "stack.h"
+#include "libraries.h"
+
+/* Notes on STACKS
+    Operators:
+        - Push: insert "something" to stack
+        - Pop: delete "something" in the stack
+
+    Attributes:
+        - top: index of the most recently inserted element
+        - size: capacity of the stack
+*/
+
+/** createStack
+ * creates an empty stack with size `S`
+ * 
+ * @params:
+ *  - S : int : the size/capacity of the stack
+ * 
+ * @returns:
+ *  - none
+ */
+void create(int S){
+
+
+}
--- stack.h
+++ stack.h
@@ -11,4 +11,56 @@
 #ifndef STACK
 #define STACK
 
+#define MAX_SIZE 256 // define the maximum size of a stack as 256
+
+/** stack
+ * struct data type for a stack
+ * 
+ * @fields:
+ *  - items[MAX_SIZE] : int : array of integers, with maximum size of MAX_SIZE = 256
+ *  - top : int : the most recent addition in the items array 
+ */
+typedef struct stack
+{
+    int items[MAX_SIZE];
+    int top;
+};
+
+
+/** createStack
+ * creates an empty stack with size `S`
+ * 
+ * @params:
+ *  - S : int : the size/capacity of the stack
+ * 
+ * @returns:
+ *  - none
+ */
+void create(int S);
+
+/**push
+ * inserts an element `elem` in the stack `Stack`.
+ * 
+ * @params:
+ *  - elem : int : the element to insert in the stack
+ *  - Stack : stack : the stack
+ * 
+ * @returns:
+ *  - 1 : for successful push
+ *  - -1 : for unsuccessful push (caused by overflow)
+ */
+int push(int elem, stack Stack);
+
+/**pop
+ * deletes the top element in the stack and returns the element
+ * 
+ * @params:
+ *  - Stack : stack : the stack
+ * 
+ * @returns:
+ *  - top : the top element; the deleted element
+ *  - -999999 : for unseccessful pop (caused by underflow)
+ */
+int pop(stack Stack);
+
 #endif
