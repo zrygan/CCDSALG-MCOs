ba26260409ec77fe628c97a65dcbfb2405f5c153
zrygan <zr.gntn@gmail.com>
2024-06-08 00:07:25 +0800

    Changed directory locations

--- src/libraries.h
+++ src/libraries.h
@@ -13,7 +13,7 @@
 
 #include <stdio.h>
 #include <stdlib.h> 
-#include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_SIZE 256 // define the maximum size of a stack as 256
 
--- src/main.c
+++ src/main.c
@@ -13,6 +13,12 @@
 #include "queue.h"
 
 int main(){
+    stack myStack = create(10);
+    push(1, myStack);
+    push(10, myStack);
+    push(56, myStack);
+    printf("%d", pop(myStack));
+    printf("%d", pop(myStack));
 
     return 0;
 }
