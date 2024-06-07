e005b01465551b5c9da6ca9bbc85b73fabd2c20a
zrygan <zr.gntn@gmail.com>
2024-06-08 01:26:37 +0800

    Added queue structure

--- queue.c
+++ queue.c
@@ -7,3 +7,67 @@
  * 
  * Source file of queue data structure .
 */
+
+#include "queue.h"
+
+/**createQueue
+ * Implementation of create queue function, see documentation in `queue.h`
+ * 
+ * @author: Zhean Ganituen
+ */
+queue createQueue(int S){
+    queue *Queue = malloc(sizeof(queue));
+
+    Queue->size = S-1; // init size as S-1 (since max index for size S is S-1)
+
+    Queue->head = 0; // start head and tail at 0 position
+    Queue->tail = 0;
+    Queue->elems = 0;
+
+    return *Queue;
+}
+
+void enqueue(int elem, queue *Queue){
+    // put the element in the tail position
+    Queue->items[Queue->tail] = elem;
+    Queue->elems++;
+
+    // update tail position
+    // move the position by 1 and make it loop around if it exeeds size
+    Queue->tail = Queue->tail++ % Queue->size + 1;
+    // dont update head
+}
+
+int dequeue(queue *Queue){
+    // store removed value
+    int removed = Queue->items[Queue->head];
+
+    // remove the element in the head position
+    Queue->items[Queue->head] = 0;
+    Queue->elems--;
+
+    // update head position
+    // same formula as with enqueue (moving tail)
+    Queue->head = Queue->head++ % Queue->size + 1;
+    // dont update tail
+
+    return removed;
+}
+
+bool queueEmpty(queue Queue){
+    if (Queue.elems == 0) return true;
+    else return false;
+}
+
+bool queueFull(queue Queue){
+    if (Queue.elems == Queue.size) return true;
+    else return false;
+}
+
+int queueHead(queue Queue){
+    return Queue.items[Queue.head];
+}
+
+int queueTail(queue Queue){
+    return Queue.items[Queue.tail];
+}
--- queue.h
+++ queue.h
@@ -11,4 +11,111 @@
 #ifndef QUEUE
 #define QUEUE
 
+#include "libraries.h"
+
+/**queue
+ * struct data type for a queue
+ * 
+ * @fields:
+ *  - items[MAX_SIZE] : int : array of integers, with maximum size of MAX_SIZE = 256
+ *  - head            : int : index where the next element to be dequeued
+ *  - tail            : int : index where the next element to be inserted
+ *  - size            : int : actual max size of the queue
+ *  - elems           : int : current number of elements in the queue
+ * 
+ * @author: Zhean Ganituen
+ */
+typedef struct{
+    int items[MAX_SIZE];
+    int head;
+    int tail;
+    int size;
+    int elems;
+} queue;
+
+/**createQueue
+ * creates an empty queue with head and tail at 0
+ * 
+ * @params:
+ *  - S : int : size of queue
+ * 
+ * @returns:
+ *  - *Queue : the queue created 
+ * 
+ * @author: Zhean Ganituen
+*/
+queue createQueue(int S);
+
+/**enqueue
+ * adds an element in the tail position
+ * 
+ * @params:
+ *  - elem  : int    : element to be insterted
+ *  - Queue : queue* : the queue
+ * 
+ * @returns:
+ *  - none
+ * 
+ * @author: Zhean Ganituen
+ */
+void enqueue(int elem, queue *Queue);
+
+/**dequeue
+ * deletes element in the head position
+ * 
+ * @params:
+ *  - Queue : queue* : the queue
+ * 
+ * @returns:
+ *  - removed : int : the element removed
+ */
+int dequeue(queue *Queue);
+
+/**queueEmpty
+ * checks if the queue is empty
+ * 
+ * @params:
+ *  - Queue : queue : the queue
+ * 
+ * @returns:
+ *  - true  : when the queue is empty
+ *  - false : otherwise
+ */
+bool queueEmpty(queue Queue);
+
+
+/**queueFull
+ * checks if the queue is full
+ * 
+ * @params:
+ *  - Queue : queue : the queue
+ * 
+ * @returns:
+ *  - true  : when the queue is full
+ *  - false : otherwise
+ */
+bool queueFull(queue Queue);
+
+/**queueHead
+ * returns the value at the head
+ * 
+ * @params:
+ *  - queue : queue : the queue
+ * 
+ * @returns:
+ *  - val : int : the value at the head
+ */
+int queueHead(queue Queue);
+
+/**queueTail
+ * returns the value at the tail
+ * 
+ * @params:
+ *  - queue : queue : the queue
+ * 
+ * @returns:
+ *  - val : int : the value at the tail
+ */
+int queueTail(queue Queue);
+
 #endif
