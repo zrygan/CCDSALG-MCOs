2b80ee2107513aa3b6ca6c86abc91ed48cb206b1
Viennbalcita <vienn_rowen_balcita@dlsu.edu.ph>
2024-06-15 23:56:00 +0800

    Primary functions completed, helper functions to be finished.

--- evalPost.c
+++ evalPost.c
@@ -7,3 +7,34 @@
  * 
  * Source file of postfix evaluation algorithm.
 */
+
+#include "queue.h"
+#include "queue.c"
+#include "stack.h"
+#include "stack.c"
+#include "inToPost.c"
+
+
+int Evaluate(int token1,int operator,int token2){
+switch(operator){
+}
+}
+
+int EvaluatePostfix(queue Postfix){
+stack Output = createStack(Postfix.elems);
+queue temporary = createQueue(Postfix.elems);
+
+while(!queueEmpty(Postfix)){
+
+    if(isOperator(queueHead(Postfix))){
+        enqueue(pop(&Output),&temporary);
+        enqueue(dequeue(&Postfix),&Postfix);
+        enqueue(pop(&Output),&temporary);
+        push(Evaluate(dequeue(&temporary),dequeue(&temporary),dequeue(&temporary)),&Output);
+    }
+    else{
+        push(dequeue(&Postfix),&Output);
+    }
+}
+return pop(&Output);
+}
--- inToPost.c
+++ inToPost.c
@@ -7,3 +7,76 @@
  * 
  * Source file of infix to postfix algorithm.
 */
+
+#include "queue.c"
+#include "queue.h"
+#include "stack.c"
+#include "stack.h"
+
+//Does not consider tokens with two characters yet
+bool isOperator(int operator){
+    if (operator == '+' || operator == '-' || operator == '/' || operator == '^' || operator == '%' || 
+    operator == '*' || operator == '<' || operator == '>'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int getPrecedence(int precedence){
+    switch(precedence){
+        case '(':
+        case ')': return 8;
+        case '!': return 7;
+        case '^': return 6;
+        case '%':
+        case '*':
+        case '/': return 5;
+        case '+':
+        case '-': return 4;
+        case '<':
+        case '>':
+        case ">=":
+        case "<=":
+        case "!=":
+        case "==": return 3;
+        case "&&": return 2;
+        case "||": return 1;
+        default: return 0;
+    }
+}
+
+queue InfixtoPostfix(queue infix){
+stack Operators = createStack(infix.elems);
+queue Postfix = createQueue(infix.elems);
+
+while(!queueEmpty(infix)){
+    //Opening Parenthesis not needed
+    if(queueHead(infix)=='('){
+        dequeue(&infix);
+    }
+    else if(isOperator(queueHead(infix))){
+        //If top of operator stack precedence is higher than head of infix queue
+        if(getPrecedence(top(Operators))>getPrecedence(queueHead(infix))){
+            //Pop the operator stack, enqueue to post fix queue
+            enqueue(pop(&Operators),&Postfix);
+        }
+        // if not, push the operator to the operator stack
+        else{
+            push(dequeue(&infix),&Operators);
+        }
+    }
+    //If the token is an operand, add it to the the postfix queue
+    else{
+        enqueue(dequeue(&infix),&Postfix);
+    }
+}
+//Any operators left in stack get enqueued to the postfix queue
+while(!stackEmpty(Operators)){
+    enqueue(pop(&Operators),&Postfix);
+}
+return Postfix;
+}
+
+//
