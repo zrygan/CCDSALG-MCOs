683aafdae317cc6da804a8fd13de12f377269eac
zrygan <zr.gntn@gmail.com>
2024-06-28 16:58:47 +0800

    Fixed dir tree and batch file for 2nd phase

---
+++ src/mco2/main.c
@@ -0,0 +1,14 @@
+/**
+ * Balcita, V.
+ * Ganituen, Z.
+ * Jimenez, J.
+ * 
+ * CCDSALG, Project 1
+ * 
+ * Main C file of Part 1 of the project.
+*/
+
+int main(){
+
+    
+}
--- zry.bat
+++ zry.bat
@@ -4,20 +4,20 @@ REM Batch file to execute C file using I/O redirection
 echo off
 
 REM Clear Screen
-
 cls
 
-REM Compile `main.c` using `gcc -Wall`
+REM Compile mco1
+REM gcc -Wall src/mco1/*.c -o main_mco1.exe
 
-gcc -Wall main.c stack.c queue.c evalPost.c inToPost.c -o main.exe
+REM Compile mco2
+gcc -Wall src/mco2/*.c -o main_mco2.exe
 
-REM Without I/O file redirection
+REM Execute mco1 with input redirection and output redirection
+REM type test\mco1\TESTCASE_1.txt | main_mco1.exe > test\mco1\RESULT_1.txt
+REM type test\mco1\TESTCASE_2.txt | main_mco1.exe > test\mco1\RESULT_2.txt
 
-REM main
-
-REM With I/O file redirection
-
-main.exe < TESTCASE_1.txt > RESULT_1.txt
-main.exe < TESTCASE_2.txt > RESULT_2.txt
+REM Execute mco2 with input redirection and output redirection (if needed)
+type test\mco2\TESTCASE_1.txt | main_mco1.exe > test\mco2\RESULT_1.txt
+type test\mco2\TESTCASE_2.txt | main_mco1.exe > test\mco2\RESULT_2.txt
 
 REM END OF FILE
