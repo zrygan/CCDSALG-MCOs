08571a53c858f31eb6277ee946bceee27cc062f9
Viennbalcita <vienn_rowen_balcita@dlsu.edu.ph>
2024-07-15 16:27:51 +0800

    Added BFS (not yet tested, not yet debugged)

--- src/mco2/traversal_BFS.h
+++ src/mco2/traversal_BFS.h
@@ -8,10 +8,19 @@
  * header file for bfs transveral of graph
 */
 
-#define TRANSVERAL_BFS
 #ifndef TRANSVERAL_BFS
+#define TRANSVERAL_BFS
 
 #include "libraries.h"
+#include "adjacency_matrix.h"
+#include "make_tree.h"
 
+
+typedef struct BFSNode
+{
+    String val;
+    struct BFSNode** neighbors;
+    int numNeighbors;
+} BFSNode;
 
 #endif
