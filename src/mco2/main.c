60446a18c2873d959462b3e3fafd77917f7867ec
zrygan <zr.gntn@gmail.com>
2024-07-28 23:50:25 +0800

    Changed main formatting and added checker in make_tree.c

--- src/mco2/main.c
+++ src/mco2/main.c
@@ -21,8 +21,8 @@ int main()
     FILE *f;                 // the file pointer
     adjacency_matrix matrix; // the adjaacency matrix
     tree_node *tree;
-    int start_index;         // start index : the index where the traversal starts
-    String start;            // start vertex
+    int start_index; // start index : the index where the traversal starts
+    String start;    // start vertex
 
     printf("Input filename: ");
     scanf("%s", fileName);
@@ -31,14 +31,14 @@ int main()
 
     if (f != NULL)
     {
-        make_adjacency_matrix(f, &matrix); 
+        make_adjacency_matrix(f, &matrix);
 
-        display_graph(matrix); // _____ADDITIONAL FEATURE_____ 
-                               // (makes the graph)
-                               // COMMENT OUT, IF NOT NEEDED
-
-        printf("Input start vertex for the traversal: ");
-        scanf("%s", start);
+        // initialize the tree
+        tree = malloc(matrix.vertex * sizeof(tree_node));
+        for (int i = 0; i < matrix.vertex; i++)
+        {
+            tree[i].checker = false;
+        }
 
         // OUTPUT: the degrees
         int *degrees = malloc(matrix.vertex * sizeof(int));
@@ -49,27 +49,35 @@ int main()
         }
         printf("\n");
 
-        tree = malloc(matrix.vertex * sizeof(tree_node));
+        printf("Input start vertex for the traversal: ");
+        scanf("%s", start);
 
         // OUTPUT: the BFS and DFS path
         for (start_index = 0; start_index < matrix.vertex; start_index++)
         { // checks if vertex exists
-            if (!strcmp(matrix.names[start_index], start))
+            if (!strcasecmp(matrix.names[start_index], start))
             {
-                BFStraversal(matrix, start_index, tree);
-                printf("\n\n");
-                DFSTraversal(matrix, start_index);
+                BFStraversal(matrix, start_index, tree); // OUTPUT: BFS
+                printf("\n\n");                          // SPACER
+                DFSTraversal(matrix, start_index);       // OUTPUT: DFS
                 
+                // _____ADDITIONAL FEATURE_____
+                // (makes the graph)
+                // COMMENT OUT, IF NOT NEEDED
+                printf("\n\nGraph:\n");
+                display_graph(matrix);
+
                 // _____ADDITIONAL FEATURE_____ <start>
                 // (makes the graph)
                 // COMMENT OUT, IF NOT NEEDED
-                printf("\n\nTree:\n");
-                for (int i = 0; i < matrix.vertex; i++) {
-                    if (tree[i].distance == 0) {
+                printf("\nTree:\n");
+                for (int i = 0; i < matrix.vertex; i++)
+                {
+                    if (tree[i].distance == 0)
+                    {
                         display_tree(tree, matrix.vertex, tree[i].name, 0);
                     }
                 }
-                // _____ADDITIONAL FEATURE_____ <end>
 
                 return 0;
             }
--- src/mco2/make_tree.h
+++ src/mco2/make_tree.h
@@ -18,6 +18,7 @@ typedef struct tree_node
     String name;
     int distance;
     String root;
+    bool checker;
 } tree_node;
 
 void display_tree(tree_node *tree, int vertices, const char *curr, int distance);
